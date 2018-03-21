5ca451f929ddf16c79253be248fd0280f85935f6
Paweł Micek <micekpwl@gmail.com>
2018-03-21 23:23:41 +0100

    Reorganize folder structure; add solution; add stub test xunit project

---
+++ F1WM.Tests/Controllers/NewsControllersTests.cs
@@ -0,0 +1,28 @@
+using F1WM.Controllers;
+using Xunit;
+
+namespace F1WM.Tests.Controllers
+{
+	public class NewsControllersTests
+	{
+		private NewsController controller;
+
+		[Fact]
+		public void ShouldReturnLast20NewsByDefault()
+		{
+
+		}
+
+		[Fact]
+		public void ShouldReturnNewsOlderThanFirstId()
+		{
+
+		}
+
+		[Fact]
+		public void ShouldReturnNewsDetailsById()
+		{
+
+		}
+	}
+}
