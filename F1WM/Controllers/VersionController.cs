fd8631097dcd951bb7257f7a018ecf64d4c1db24
Paweł Micek <micekpwl@gmail.com>
2018-03-28 00:28:35 +0200

    Add build number tracking

--- F1WM/Controllers/VersionController.cs
+++ F1WM/Controllers/VersionController.cs
@@ -1,17 +1,25 @@
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 using F1WM.Model;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace F1WM.Controllers
 {
 	[Route("api/[controller]")]
 	public class VersionController : Controller
 	{
-		private readonly ApiVersion version = new ApiVersion() { Version = "0.0.1" };
-
 		[HttpGet]
 		public ApiVersion GetVersion()
 		{
-			return version;
+			var assembly = Assembly.GetEntryAssembly();
+			var resourceStream = assembly.GetManifestResourceStream("F1WM.version.json");
+			using (var reader = new StreamReader(resourceStream, Encoding.UTF8))
+			{
+				return JsonConvert.DeserializeObject<ApiVersion>(reader.ReadToEnd());
+			}
 		}
 	}
 }
---
+++ F1WM/scripts/add_build_number.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATE=`date +%Y-%m-%d-%H%M`
+sed "s/<build_number>/$DATE/g" scripts/version.template.json > version.json
