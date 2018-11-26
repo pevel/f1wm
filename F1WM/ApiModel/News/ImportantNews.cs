165d4ae9b3a5ea87173f6606964bd69743783a7a
Paweł Micek <micekpwl@gmail.com>
2018-11-26 20:08:06 +0100

    Move news-related model to a separate folder; add model for hot news

---
+++ F1WM/ApiModel/News/ImportantNews.cs
@@ -0,0 +1,10 @@
+namespace F1WM.ApiModel
+{
+	public class ImportantNewsSummary
+	{
+		public int Id { get; set; }
+		public string ImageUrl { get; set; }
+		public string ShortText { get; set; }
+		public string Title { get; set; }
+	}
+}
