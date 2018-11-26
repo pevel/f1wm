c9ee2627455ab17ad8448620c4a09e5c23fc1174
Paweł Micek <micekpwl@gmail.com>
2018-11-26 19:46:02 +0100

    Fix typo

--- F1WM/ApiModel/RaceResultPosition.cs
+++ F1WM/ApiModel/RaceResultPosition.cs
@@ -9,7 +9,7 @@ namespace F1WM.ApiModel
 		public int Number { get; set; }
 		public DriverSummary Driver { get; set; }
 		public CarSummary Car { get; set; }
-		public string Tires { get; set; }
+		public string Tyres { get; set; }
 		public int FinishedLaps { get; set; }
 		public TimeSpan Time { get; set; }
 		public int PitStopVisits { get; set; }
