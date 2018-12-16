c743a120224520fe091bbe1cd0d4d3f148cb5d79
Paweł Micek <micekpwl@gmail.com>
2018-12-16 21:47:06 +0100

    Ignore null values when nationality information is redundant

--- F1WM/ApiModel/ConstructorSummary.cs
+++ F1WM/ApiModel/ConstructorSummary.cs
@@ -1,8 +1,11 @@
+using Newtonsoft.Json;
+
 namespace F1WM.ApiModel
 {
 	public class ConstructorSummary
 	{
 		public string Name { get; set; }
+		[JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
 		public Country Nationality { get; set; }
 	}
 }
