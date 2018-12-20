1cec1117681abc8b4a074f28dddde75d9dda803b
pevel <pawel.micek-ext@leica-geosystems.com>
2018-12-20 15:07:46 +0100

    Create initial API model

--- F1WM/ApiModel/Results/QualifyingResult.cs
+++ F1WM/ApiModel/Results/QualifyingResult.cs
@@ -1,9 +1,13 @@
 using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace F1WM.ApiModel
 {
 	public class QualifyingResult
 	{
-		
+		public IEnumerable<QualifyingResultPosition> Results { get; set; }
+		public LapResultSummary FastestLap { get; set; }
 	}
 }
---
+++ F1WM/ApiModel/Results/QualifyingResultPosition.cs
@@ -0,0 +1,14 @@
+namespace F1WM.ApiModel
+{
+	public class QualifyingResultPosition
+	{
+		public int FinishPosition { get; set; }
+		public int Number { get; set; }
+		public DriverSummary Driver { get; set; }
+		public CarSummary Car { get; set; }
+		public int FinishedLaps { get; set; }
+		public QualifyingSessionResultPosition Session1 { get; set; }
+		public QualifyingSessionResultPosition Session2 { get; set; }
+		public QualifyingSessionResultPosition Session3 { get; set; }
+	}
+}
