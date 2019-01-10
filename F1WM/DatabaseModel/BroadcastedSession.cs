40cfd2eff5bb05952e0bd591742ca9570fc8fca3
pevel <pawel.micek-ext@leica-geosystems.com>
2019-01-10 14:42:46 +0100

    Add migration; improve database model

--- F1WM/DatabaseModel/BroadcastedSession.cs
+++ F1WM/DatabaseModel/BroadcastedSession.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace F1WM.DatabaseModel
 {
 	public class BroadcastedSession
 	{
 		public int Id { get; set; }
+		[MaxLength(255)]
 		public string Name { get; set; }
 		public DateTime Start { get; set; }
-		public virtual IEnumerable<Broadcast> Broadcasts { get; set;}
+		public virtual IEnumerable<Broadcast> Broadcasts { get; set; }
 	}
 }
---
+++ F1WM/Migrations/F1WM/20190110125118_Broadcasts.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace F1WM.Migrations.F1WM
+{
+    public partial class Broadcasts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("ALTER TABLE `f1races` CHANGE `date` `date` DATE NOT NULL;");
+            migrationBuilder.Sql("ALTER TABLE `f1races` ENGINE=InnoDB;");
+            migrationBuilder.CreateTable(
+                name: "BroadcastedSessions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 255, nullable: true),
+                    Start = table.Column<DateTime>(nullable: false),
+                    RaceId = table.Column<uint>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BroadcastedSessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BroadcastedSessions_f1races_RaceId",
+                        column: x => x.RaceId,
+                        principalTable: "f1races",
+                        principalColumn: "raceid",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Broadcasters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Url = table.Column<string>(maxLength: 255, nullable: true),
+                    Name = table.Column<string>(maxLength: 255, nullable: true),
+                    Icon = table.Column<string>(maxLength: 255, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Broadcasters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Broadcasts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    BroadcasterId = table.Column<int>(nullable: false),
+                    BroadcastedSessionId = table.Column<int>(nullable: false),
+                    Start = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Broadcasts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Broadcasts_BroadcastedSessions_BroadcastedSessionId",
+                        column: x => x.BroadcastedSessionId,
+                        principalTable: "BroadcastedSessions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Broadcasts_Broadcasters_BroadcasterId",
+                        column: x => x.BroadcasterId,
+                        principalTable: "Broadcasters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BroadcastedSessions_RaceId",
+                table: "BroadcastedSessions",
+                column: "RaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Broadcasts_BroadcastedSessionId",
+                table: "Broadcasts",
+                column: "BroadcastedSessionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Broadcasts_BroadcasterId",
+                table: "Broadcasts",
+                column: "BroadcasterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Broadcasts");
+
+            migrationBuilder.DropTable(
+                name: "BroadcastedSessions");
+
+            migrationBuilder.DropTable(
+                name: "Broadcasters");
+        }
+    }
+}
