5e75b18b8dc430b9662a073239bc2c8d8c6b3c7d
Filipki <filipkieres@gmail.com>
2019-01-13 01:24:51 +0100

    clen up  mapper

--- F1WM/Mapping/NewsMappingProfile.cs
+++ F1WM/Mapping/NewsMappingProfile.cs
@@ -9,9 +9,8 @@ namespace F1WM.Mapping
 	{
 		public NewsMappingProfile()
 		{
-			CreateMap<News, NewsSummary>()
-				.ForMember(api => api.TopicIcon, o => o.MapFrom(db => db.Topic.Icon));
+            CreateMap<News, NewsSummary>();
 			CreateMap<News, NewsDetails>();
-		}
+        }
 	}
 }
