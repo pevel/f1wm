7c35904e65b992c48f130267b1678ae62ac22712
Filipki <filipkieres@gmail.com>
2019-01-17 00:21:53 +0100

    Add class for pagination

---
+++ F1WM/ApiModel/News/NewsSummaryPaged.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace F1WM.ApiModel
+{
+	public class NewsSummaryPaged
+	{
+		public int CurrentPage { get; set; }
+		public int PageCount { get; set; }
+		public int PageSize { get; set; }
+		public int RowCount { get; set; }
+		public IEnumerable<NewsSummary> Result { get; set; }
+	}
+}
