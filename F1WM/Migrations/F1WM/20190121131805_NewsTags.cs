57cd906a6ec4ea8674e5e53ce9bd1daa2a4366e8
pevel <pawel.micek-ext@leica-geosystems.com>
2019-01-21 14:25:55 +0100

    Add migration

---
+++ F1WM/Migrations/F1WM/20190121131805_NewsTags.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace F1WM.Migrations.F1WM
+{
+    public partial class NewsTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "topic_icon",
+                table: "f1_news_topics");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_f1_news_topicmatch_f1_news_news_id",
+                table: "f1_news_topicmatch",
+                column: "news_id",
+                principalTable: "f1_news",
+                principalColumn: "news_id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_f1_news_topicmatch_f1_news_topics_topic_id",
+                table: "f1_news_topicmatch",
+                column: "topic_id",
+                principalTable: "f1_news_topics",
+                principalColumn: "topic_id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_f1_news_topicmatch_f1_news_news_id",
+                table: "f1_news_topicmatch");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_f1_news_topicmatch_f1_news_topics_topic_id",
+                table: "f1_news_topicmatch");
+
+            migrationBuilder.AddColumn<string>(
+                name: "topic_icon",
+                table: "f1_news_topics",
+                maxLength: 20,
+                nullable: true);
+        }
+    }
+}
