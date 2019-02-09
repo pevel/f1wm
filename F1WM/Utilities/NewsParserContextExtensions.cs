8acba24a219219026270c11307a96e7611f30c9a
Paweł Micek <micekpwl@gmail.com>
2019-02-09 22:48:03 +0100

    Improve table parsing

--- F1WM/Utilities/NewsParserContextExtensions.cs
+++ F1WM/Utilities/NewsParserContextExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using F1WM.Utilities.Model;
 
@@ -6,15 +7,15 @@ namespace F1WM.Utilities
 {
 	public static class NewsParserContextExtensions
 	{
+		private const string tableClass = "news-table";
+		private const string titleClass = "news-table-title";
+		private const string footerClass = "news-table-footer";
+
 		public static void ParseTable(this NewsParserContext context)
 		{
-			if (IsTableDefinition(context.CurrentLine) && context.Reader.Peek() != -1)
+			if (TryParseTableDeclaration(context.CurrentLine, out var tableProperties) && context.Reader.Peek() != -1)
 			{
-				context.Writer.Write("<table><tr>");
-				ParseTableHeaders(context);
-				context.Writer.Write("</tr>");
-				ParseTableBody(context);
-				context.Writer.Write("</table>");
+				ParseTable(context, tableProperties);
 			}
 			else
 			{
@@ -22,43 +23,72 @@ namespace F1WM.Utilities
 			}
 		}
 
-		private static bool IsTableDefinition(string text)
+		private static bool TryParseTableDeclaration(string text, out NewsTableProperties properties)
 		{
-			var regex = new Regex(@"%MAX%(\d)%");
-			return regex.Matches(text).Count != 0;
-		}
-
-		private static void ParseTableHeaders(NewsParserContext context)
-		{
-			var headersLine = context.Reader.ReadLine();
-			var tokens = headersLine.Split('|');
-			if (tokens.Length > 1)
+			properties = null;
+			var regex = new Regex(@"%(?<width>\w+)%(?<columns>\d+)%(?<order>NR)?%?(STRATA)?%?(\w+)?");
+			var match = regex.Match(text);
+			if (match.Success)
 			{
-				foreach (var token in tokens)
+				properties = new NewsTableProperties
 				{
-					var header = token == "&" ? "" : token;
-					context.Writer.Write($"<th>{header}</th>");
-				}
+					Width = match.Groups["width"].Value,
+					InsertOrderColumn = match.Groups["order"].Success,
+					ColumnsCount = Int32.Parse(match.Groups["columns"].Value)
+				};
+				return true;
 			}
-			else
-			{
-				throw new ArgumentException(
-					"Attempted to parse news table in an unknown format",
-					nameof(context));
-			}
+			return false;
 		}
 
-		private static void ParseTableBody(NewsParserContext context)
+		private static void ParseTable(NewsParserContext context, NewsTableProperties properties)
 		{
+			bool expectHeaders = false;
+			int order = 1;
+			context.Writer.Write($"<table class=\"{tableClass}\">");
 			while (context.Reader.Peek() != -1)
 			{
 				var line = context.Reader.ReadLine();
-				var tokens = line.Split('|');
+				var tokens = line.Split('|').Take(properties.ColumnsCount).ToArray();
 				if (tokens.Length > 1)
 				{
-
+					context.Writer.Write("<tr>");
+					if (tokens[0] == "&" || tokens[0] == "%")
+					{
+						string className = tokens[0] == "&" ? titleClass : footerClass;
+						context.Writer.Write($"<td class=\"{className}\">{tokens[1]}</td>");
+						expectHeaders = true;
+						order = 1;
+					}
+					else
+					{
+						string tag = expectHeaders ? "th" : "td";
+						if (properties.InsertOrderColumn)
+						{
+							string toWrite = expectHeaders ? "" : order.ToString();
+							context.Writer.Write($"<{tag}>{toWrite}</{tag}>");
+						}
+						foreach (var token in tokens)
+						{
+							string toWrite = token;
+							if (toWrite.StartsWith('~'))
+							{
+								toWrite = $"<img src=\"{toWrite.Substring(1).GetFlagIconPath()}\">";
+							}
+							context.Writer.Write($"<{tag}>{toWrite}</{tag}>");
+						}
+						expectHeaders = false;
+					}
+					context.Writer.Write("</tr>");
+				}
+				else
+				{
+					context.Writer.Write("</table>");
+					context.Writer.Write(line);
+					return;
 				}
 			}
+			context.Writer.Write("</table>");
 		}
 	}
 }
