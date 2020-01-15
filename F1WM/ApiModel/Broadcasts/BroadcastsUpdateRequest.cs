987303650174c363431cb38130cc4ea5d253e0ca
pevel <micekpwl@gmail.com>
2020-01-15 14:19:36 +0100

    Remove ID from broadcasts general update request

--- F1WM/ApiModel/Broadcasts/BroadcastsUpdateRequest.cs
+++ F1WM/ApiModel/Broadcasts/BroadcastsUpdateRequest.cs
@@ -6,8 +6,6 @@ namespace F1WM.ApiModel
 	public class BroadcastsUpdateRequest
 	{
 		[Required]
-		public int Id { get; set; }
-		[Required]
 		public int RaceId { get; set; }
 		[Required]
 		public IEnumerable<BroadcastedSessionAddRequest> Sessions { get; set; }
