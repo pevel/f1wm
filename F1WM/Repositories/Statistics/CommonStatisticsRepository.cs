3549219bf0b8079d00eba015ec545991373ae9e1
pevel <micekpwl@gmail.com>
2020-04-27 12:12:40 +0200

    Rewrite queries to adapt to EF Core changes

--- F1WM/Repositories/Statistics/CommonStatisticsRepository.cs
+++ F1WM/Repositories/Statistics/CommonStatisticsRepository.cs
@@ -19,7 +19,7 @@ namespace F1WM.Repositories.Statistics
 
 		public async Task<IEnumerable<TeamsSeason>> GetTeams(int driverId, int atYear)
 		{
-			return await context.Entries
+			return (await context.Entries
 				.Where(e => e.DriverId == driverId)
 				.Where(e => e.Race.Date.Year <= atYear)
 				.Select(e => new
@@ -27,20 +27,20 @@ namespace F1WM.Repositories.Statistics
 					SeasonId = e.Race.SeasonId,
 					Team = e.Team
 				})
+				.ToListAsync())
 				.GroupBy(e => new { e.SeasonId, e.Team.Id })
 				.Select(e => new TeamsSeason
 				{
 					SeasonId = e.Key.SeasonId,
 					Team = mapper.Map<TeamSummary>(e.Select(g => g.Team).First())
-				})
-				.ToListAsync();
+				});
 		}
 
 		public async Task<IEnumerable<DriversSeason>> GetDrivers(
 			Expression<Func<Entry, bool>> selector,
 			int atYear)
 		{
-			return await context.Entries
+			return (await context.Entries
 				.Where(selector)
 				.Where(e => e.Race.Date.Year <= atYear)
 				.Select(e => new
@@ -49,20 +49,20 @@ namespace F1WM.Repositories.Statistics
 					TeamId = e.TeamId,
 					Driver = e.Driver
 				})
+				.ToListAsync())
 				.GroupBy(e => new { e.SeasonId, e.TeamId, e.Driver.Id })
 				.Select(g => new DriversSeason
 				{
 					SeasonId = g.Key.SeasonId,
 					Driver = mapper.Map<DriverSummary>(g.Select(entry => entry.Driver).FirstOrDefault())
-				})
-				.ToListAsync();
+				});
 		}
 
 		public async Task<IEnumerable<CarsSeason>> GetCars(
 			Expression<Func<Entry, bool>> selector,
 			int atYear)
 		{
-			return await context.Entries
+			return (await context.Entries
 				.Where(selector)
 				.Where(e => e.Race.Date.Year <= atYear)
 				.Select(e => new
@@ -71,81 +71,90 @@ namespace F1WM.Repositories.Statistics
 					TeamId = e.TeamId,
 					Car = e.Car
 				})
+				.ToListAsync())
 				.GroupBy(e => new { e.SeasonId, e.TeamId, e.Car.Id })
 				.Select(g => new CarsSeason
 				{
 					SeasonId = g.Key.SeasonId,
 					Car = mapper.Map<CarSummary>(g.Select(entry => entry.Car).FirstOrDefault())
-				})
-				.ToListAsync();
+				});
 		}
 
 		public async Task<IEnumerable<PointsStatistics>> GetPointsStatistics(
 			Expression<Func<DriverPoints, bool>> selector,
 			int atYear)
 		{
-			return await context.DriverPoints
+			return (await context.DriverPoints
 				.Where(selector)
 				.Where(p => p.Race.Date.Year <= atYear)
+				.ToListAsync())
 				.GroupBy(p => p.SeasonId)
 				.Select(g => new PointsStatistics
 				{
 					SeasonId = g.Key,
 					Points = g.Sum(p => p.Points ?? 0) + g.Sum(p => p.NotCountedTowardsChampionshipPoints ?? 0),
 					AnyPoints = g.Count()
-				})
-				.ToListAsync();
+				});
 		}
 
 		public async Task<IEnumerable<GridStatistics>> GetGridStatistics(
 			Expression<Func<Grid, bool>> selector,
 			int atYear)
 		{
-			return await context.Grids
+			return (await context.Grids
 				.Where(selector)
 				.Where(g => g.Race.Date.Year <= atYear)
 				.Where(g => g.Entry.Result.PositionOrStatus != DatabaseConstants.ResultStatus.DidNotStart)
-				.GroupBy(r => r.Race.SeasonId)
-				.Select(g => new GridStatistics
-				{
-					SeasonId = g.Key,
-					PolePositions = g.Count(grid => grid.StartPositionOrStatus == "1"),
-					GrandPrixCount = g.GroupBy(grid => grid.RaceId)
-						.Count(raceGrids => raceGrids.Any(raceGrid => raceGrid.Started())),
-					Starts = g.Count(grid => grid.Started())
+				.Select(g => new {
+					Grid = g,
+					SeasonId = g.Race.SeasonId
 				})
-				.ToListAsync();
+				.ToListAsync())
+				.GroupBy(g => g.SeasonId)
+				.Select(group => new GridStatistics
+				{
+					SeasonId = group.Key,
+					PolePositions = group.Count(g => g.Grid.StartPositionOrStatus == "1"),
+					GrandPrixCount = group.GroupBy(g => g.Grid.RaceId)
+						.Count(raceGrids => raceGrids.Any(raceGrid => raceGrid.Grid.Started())),
+					Starts = group.Count(grid => grid.Grid.Started())
+				});
 		}
 
 		public async Task<IEnumerable<ResultsStatistics>> GetResultsStatistics(
 			Expression<Func<Result, bool>> selector,
 			int atYear)
 		{
-			return await context.Results
+			return (await context.Results
 				.Where(selector)
 				.Where(r => r.Race.Date.Year <= atYear)
-				.GroupBy(r => r.Race.SeasonId)
-				.Select(g => new ResultsStatistics
-				{
-					SeasonId = g.Key,
-					Wins = g.Count(r => r.PositionOrStatus == "1"),
-					Podiums = g.Count(r => r.PositionOrStatus.OnPodium()),
-					NotFinished = g.Count(r => r.PositionOrStatus.NotFinished()),
-					NotClassified = g.Count(r => r.PositionOrStatus.NotClassified())
+				.Select(r => new {
+					Result = r,
+					SeasonId = r.Race.SeasonId
 				})
-				.ToListAsync();
+				.ToListAsync())
+				.GroupBy(r => r.SeasonId)
+				.Select(group => new ResultsStatistics
+				{
+					SeasonId = group.Key,
+					Wins = group.Count(r => r.Result.PositionOrStatus == "1"),
+					Podiums = group.Count(r => r.Result.PositionOrStatus.OnPodium()),
+					NotFinished = group.Count(r => r.Result.PositionOrStatus.NotFinished()),
+					NotClassified = group.Count(r => r.Result.PositionOrStatus.NotClassified())
+				});				
 		}
 
 		public async Task<IEnumerable<FastestLapsStatistics>> GetFastestLapsStatistics(
 			Expression<Func<FastestLap, bool>> selector,
 			 int atYear)
 		{
-			return await context.FastestLaps
+			return (await context.FastestLaps
 				.Where(selector)
 				.Where(f => f.PositionOrStatus == "1" && f.Race.Date.Year <= atYear)
-				.GroupBy(f => f.Race.SeasonId)
-				.Select(g => new FastestLapsStatistics { SeasonId = g.Key, FastestLaps = g.Count() })
-				.ToListAsync();
+				.Select(f => new { SeasonId = f.Race.SeasonId })
+				.ToListAsync())
+				.GroupBy(f => f.SeasonId)
+				.Select(group => new FastestLapsStatistics { SeasonId = group.Key, FastestLaps = group.Count() });
 		}
 
 		public CommonStatisticsRepository(F1WMContext context, IMapper mapper)
